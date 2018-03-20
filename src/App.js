b1d587b29c8d5149e2875e79636c92d57d2a66e2
JacksonMcgrath <jacksonfmcgrath@gmail.com>
2018-03-20 12:04:46 -1100

    finished trees

--- src/App.js
+++ src/App.js
@@ -72,7 +72,9 @@ class App extends Component {
   }
 
   handleScroll = () => {
-    // LEFT
+
+    // LEFT SIDE
+
     let tree1 = document.querySelector(".tree1");
     tree1.style.margin = -(window.pageYOffset * 1.75) + 'px ' + -(window.pageYOffset * 1.55) + 'px';
     tree1.style.width = (500 + (window.pageYOffset * .85)) + 'px';
@@ -80,17 +82,29 @@ class App extends Component {
     let tree3 = document.querySelector(".tree3");
     tree3.style.margin = (200 -(window.pageYOffset * 1.25)) + 'px ' + -(window.pageYOffset * 1.25) + 'px';
     tree3.style.width = (400 + (window.pageYOffset * .65)) + 'px';
+    tree3.style.height = (400 + (window.pageYOffset * .65)) + 'px';
 
-    // RIGHT
+    let tree5 = document.querySelector(".tree5");
+    tree5.style.margin = (380 -(window.pageYOffset * .95)) + 'px ' + -(window.pageYOffset * .85) + 'px';
+    tree5.style.width = (370 + (window.pageYOffset * .85)) + 'px';
+    tree5.style.height = (370 + (window.pageYOffset * .85)) + 'px';
+
+    // RIGHT SIDE
+
     let tree2 = document.querySelector(".tree2");
     tree2.style.margin = -(window.pageYOffset * 2.5) + 'px ' + (window.pageYOffset * .15) + 'px';
     tree2.style.width = (500 + (window.pageYOffset * .85)) + 'px';
     tree2.style.height = (500 + (window.pageYOffset * .85)) + 'px';
 
     let tree4 = document.querySelector(".tree4");
-    tree4.style.margin = (200 -(window.pageYOffset * 1.15)) + 'px ' + (window.pageYOffset * .35) + 'px';
+    tree4.style.margin = (250 -(window.pageYOffset * 1.15)) + 'px ' + (window.pageYOffset * .55) + 'px';
     tree4.style.width = (500 + (window.pageYOffset * .65)) + 'px';
     tree4.style.height = (500 + (window.pageYOffset * .65)) + 'px';
+
+    let tree6 = document.querySelector(".tree6");
+    tree6.style.margin = (540 -(window.pageYOffset * .95)) + 'px ' + -(window.pageYOffset * .75) + 'px';
+    tree6.style.width = (300 + (window.pageYOffset * .65)) + 'px';
+    tree6.style.height = (300 + (window.pageYOffset * .65)) + 'px';
 
   }
 
--- src/Story.js
+++ src/Story.js
@@ -17,10 +17,12 @@ class Story extends Component {
 				<div className="float-left">
 					<div className="tree1"></div>
 					<div className="tree3"></div>
+					<div className="tree5"></div>
 				</div>
 				<div className="float-right">
 					<div className="tree2"></div>
 					<div className="tree4"></div>
+					<div className="tree6"></div>
 				</div>
 			</div>
 

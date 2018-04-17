1c978dd8cb0385a40145d7b1bcf8847e53b6ec91
JacksonMcgrath <jacksonfmcgrath@gmail.com>
2018-04-16 17:48:12 -1100

    fixed scroll bug and about/login spacing

--- src/About.js
+++ src/About.js
@@ -17,6 +17,8 @@ class About extends Component {
 				<h2 className="half-label">Welcome to Afya Bars</h2>
 
 				<p className="half-paragraph">The main goal of Afya Bars is to bring thoughtfully sourced ingredients to health enthusiasts and give them the tools to build their perfect health bar. Our customers care about what they are putting in their body so we offer complete control and transparency with all of the ingredients we use. Let Afya bar help you along your health journey. </p>
+				<br/>
+				<br/>
 			</div>
 		)
 	}
--- src/App.js
+++ src/App.js
@@ -12,14 +12,6 @@ import About from './About'
 
 class App extends Component {
 
-  componentDidMount(){
-    window.addEventListener("scroll", this.handleScroll)
-  }
-
-  componentWillUnmount(){
-    window.removeEventListener("scroll", this.handleScroll)
-  }
-
   constructor(){
     super()
   
@@ -31,7 +23,6 @@ class App extends Component {
     this.toggleAbout = this.toggleAbout.bind(this)
     this.toggleStory = this.toggleStory.bind(this)
     this.toggleLogin = this.toggleLogin.bind(this)
-    this.handleScroll = this.handleScroll.bind(this)
   }
 
   toggleStory = () => {
@@ -60,52 +51,6 @@ class App extends Component {
     this.setState({
       render: 'login'
     })
-  }
-
-  handleScroll = () => {
-
-    // LEFT SIDE
-
-    let tree1 = document.querySelector(".tree1");
-    tree1.style.margin = -(window.pageYOffset * 2.85) + 'px ' + -(window.pageYOffset * 1.55) + 'px';
-    tree1.style.width = (500 + (window.pageYOffset * .85)) + 'px';
-
-    let tree3 = document.querySelector(".tree3");
-    tree3.style.margin = (200 -(window.pageYOffset * 1.25)) + 'px ' + -(window.pageYOffset * 1.25) + 'px';
-    tree3.style.width = (400 + (window.pageYOffset * .65)) + 'px';
-    tree3.style.height = (400 + (window.pageYOffset * .65)) + 'px';
-
-    let tree5 = document.querySelector(".tree5");
-    tree5.style.margin = (380 -(window.pageYOffset * .95)) + 'px ' + -(window.pageYOffset * .85) + 'px';
-    tree5.style.width = (370 + (window.pageYOffset * .85)) + 'px';
-    tree5.style.height = (370 + (window.pageYOffset * .85)) + 'px';
-
-    // RIGHT SIDE
-
-    let tree2 = document.querySelector(".tree2");
-    tree2.style.margin = -(window.pageYOffset * 2.5) + 'px ' + (window.pageYOffset * .15) + 'px';
-    tree2.style.width = (500 + (window.pageYOffset * .85)) + 'px';
-    tree2.style.height = (500 + (window.pageYOffset * .85)) + 'px';
-
-    let tree4 = document.querySelector(".tree4");
-    tree4.style.margin = (250 -(window.pageYOffset * 1.15)) + 'px ' + (window.pageYOffset * .55) + 'px';
-    tree4.style.width = (500 + (window.pageYOffset * .65)) + 'px';
-    tree4.style.height = (500 + (window.pageYOffset * .65)) + 'px';
-
-    let tree6 = document.querySelector(".tree6");
-    tree6.style.margin = (540 -(window.pageYOffset * .95)) + 'px ' + -(window.pageYOffset * .75) + 'px';
-    tree6.style.width = (300 + (window.pageYOffset * .65)) + 'px';
-    tree6.style.height = (300 + (window.pageYOffset * .65)) + 'px';
-
-    // OTHER LAYERS
-
-    let hill1 = document.querySelector(".hill1");
-    // hill1.style.margin = (100 + (window.pageYOffset * .45)) + 'px 0 0 0';
-
-    let tagline = document.querySelector(".tagline");
-    tagline.style.margin = (window.pageYOffset * .15) + 'px 0 0 0';
-
-
   }
 
   render() {
